f4afc8d671fe142758f369a780188adf183c307c
VijendraSinghChandel <nkysingh01@gmail.com>
2019-02-16 19:49:53 +0530

    dfdfd

--- college/static/collegeapp/college_save.js
+++ college/static/collegeapp/college_save.js
@@ -1,5 +1,31 @@
 function college_save1(){
-    $.ajax({
+var flag=true;
+if ($('#college_Name').val() == ''){
+    flag=false;
+    $("#college_name_error").css("display","block");
+    }
+else{
+     $("#college_name_error").css("display","none");
+}
+
+if ($('#street').val() == ''){
+    flag=false;
+    $("#street_error").css("display","block");
+    }
+else{
+     $("#street_error").css("display","none");
+}
+if ($('#city').val() == ''){
+    flag=false;
+    $("#city_error").css("display","block");
+    }
+else{
+     $("#city_error").css("display","none");
+}
+
+
+if (flag){
+     $.ajax({
         type:'POST',
         url:'/collegeapp/save-college/',
         data: $('#formid').serialize(),
@@ -11,5 +37,30 @@ function college_save1(){
             error:function (response)
             {
             }
+
     })
+    }
+else{
+    return false;
+    }
+
+
 }
+
+//
+//function college_save1(){
+//    $.ajax({
+//        type:'POST',
+//        url:'/collegeapp/save-college/',
+//        data: $('#formid').serialize(),
+//        success:function (response)
+//        {
+//            console.log (response);
+//            alert(response.success );
+//        },
+//            error:function (response)
+//            {
+//            }
+//
+//    })
+//}
--- college/static/studentapp/student_save.js
+++ college/static/studentapp/student_save.js
@@ -1,16 +1,67 @@
 function student_save1(){
+var flag=true;
+if ($('#name').val() == ''){
+    flag=false;
+    $("#name_error").css("display","block");
+    }
+else{
+     $("#name_error").css("display","none");
+}
+
+if ($('#contact_no').val() == ''){
+    flag=false;
+    $("#contact_error").css("display","block");
+    }
+else{
+     $("#contact_error").css("display","none");
+}
+
+alert($('#college_list_id').val())
+if ($('#college_list_id').val() == ''){
+    flag=false;
+    $("#college_error").css("display","block");
+    }
+else{
+     $("#college_error").css("display","none");
+}
+if ($('#city').val() == ''){
+    flag=false;
+    $("#city_error").css("display","block");
+    }
+else{
+     $("#city_error").css("display","none");
+}
+
+if ($('#branch').val() == ''){
+    flag=false;
+    $("#branch_error").css("display","block");
+    }
+else{
+     $("#branch_error").css("display","none");
+}
+if ($('#gender').val() == ''){
+    flag=false;
+    $("#gender_error").css("display","block");
+    }
+else{
+     $("#gender_error").css("display","none");
+}
+
+
+
+if (flag){
     $.ajax({
         type:'POST',
         url:'/studentapp/save-student/',
         data:$('#student').serialize(),
         success:function (response)
         {
-            if (response.success == 'error'){
-                alert('error');
-            }
-            else{
-                alert(response.success);
-            }
+//            if (response.success == 'error'){
+//                alert('error');
+//            }
+//            else{
+//                alert(response.success);
+//            }
 
             console.log(response);
             alert(response.success);
@@ -19,4 +70,5 @@ function student_save1(){
             {
             }
     })
+}
 }
